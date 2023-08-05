e604442b130cf8106c6145db2119d1d6eef1c729
DawalaDeny <92683913+DawalaDeny@users.noreply.github.com>
2023-08-05 18:59:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const amount = 11
 if (amount  < 10){
     console.log("Smaller than 10");
 }else{
-    console.log("bigger or equaaaal to 10");
+    console.log("Bigger or equal to 10");
 }
-console.log("Hello worldd")
+console.log("Hello world")
 
