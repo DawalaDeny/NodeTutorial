743cc3c433198c3643d99fd527a9add40238f374
DawalaDeny <shabouevdeny@gmail.com>
2023-08-06 17:54:38 +0200

    rename

--- 1-intro.js
+++ 1-intro.js
@@ -1,19 +1,8 @@
-console.log(__dirname)
-console.log(__filename)
-
-//?
-//console.log(require)
-
-//info over huidige module (file)
-//console.log(module)
-
-//info over env (waar programma uitgevoerd wordt)
-//console.log(process)
-
-setInterval(() => {
-    name()
-}, 1000);
-
-function name(params) {
-    console.log("hello world")
+const amount = 11
+if (amount  < 10){
+    console.log("Smaller than 10");
+}else{
+    console.log("Bigger or equal to 10");
 }
+console.log("Hello world, test push, aa")
+
