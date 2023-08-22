70c1b31f1a313c5a2015f968cb4f57144ba7327d
DawalaDeny <shabouevdeny@gmail.com>
2023-08-22 15:20:16 +0200

    http / packages/modules & git ignore

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
-const { log } = require('console');
-const { readFile, writeFile } = require ('fs')
-readFile('subfolder/first.txt','utf8', (err,result)=>{
-    if (err){
-        console.log(err);
-        return
-    }else{
-        const first = result;
-        readFile('subfolder/test.txt','utf8', 
-        (err, result)=>{
-            if (err){
-                console.log(err);
-                return
-            }else{
-                const second = result;
-                writeFile('subfolder/resultsync2-txt', 
-                `resultaat 2: ${first}, ${second}`,
-                (err, result)=>{
-                    if (err){
-                        console.log(err);
-                    return;
-                    }
-                    console.log(result);
-                })}
-        })
-    }
-})
+const _ = require('lodash');
+const items = [1,[2,[3,[4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
+console.log(items);
