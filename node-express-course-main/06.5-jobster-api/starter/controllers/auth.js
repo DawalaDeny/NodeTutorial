f3b84ff2f68ba3659c9156e4187960d60175162f
DawalaDeny <shabouevdeny@gmail.com>
2023-09-07 16:50:00 +0200

    begin jobs

--- node-express-course-main/06.5-jobster-api/starter/controllers/auth.js
+++ node-express-course-main/06.5-jobster-api/starter/controllers/auth.js
@@ -5,7 +5,27 @@ const { BadRequestError, UnauthenticatedError } = require('../errors')
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
   const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+  res.status(StatusCodes.CREATED).json({ user: { email:user.email, lastName:user.lastName, location:user.location
+   ,name:user.name, token:token}})
+}
+const updateUser = async (req, res) => {
+  //const user = await User.findOneAndUpdate({email:req.body.email},{...req.body})
+  //res.status(StatusCodes.ACCEPTED).json({user:{name:user.name, lastname:user.lastname}})
+  const {email, name, lastName, location}  = req.body;
+  console.log(email, name, lastName, location);
+  if (!email || !name || !lastName || !location){
+    throw new error.BadRequestError('missing value')
+  }
+  const user = await User.findOne({_id:req.user.userId});
+  console.log(user);
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+  await user.save();
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({user:{email:email, name:name, lastName:lastName, location:location, token:token}})
+
 }
 
 const login = async (req, res) => {
@@ -24,10 +44,13 @@ const login = async (req, res) => {
   }
   // compare password
   const token = user.createJWT()
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+  res.status(StatusCodes.OK).json({ user: { email:user.email, lastname:user.lastname, location:user.location
+    ,name:user.name, token:token}})
 }
 
 module.exports = {
   register,
   login,
+  updateUser,
+
 }
