18a95641ee08c051477a7f58e68bea8a734032be
denyshabouev <151159372+denyshabouev@users.noreply.github.com>
2024-02-12 16:03:43 +0100

    begin 07-fileupload

---
+++ node-express-course-main/07-file-upload/starter/controllers/productController.js
@@ -0,0 +1,17 @@
+const Product = require ('../models/Product')
+const {StatusCodes} = require('http-status-codes')
+
+const createProduct = async (req, res) => {
+    console.log(req.body);
+    const product = await Product.create(req.body);
+    res.status(StatusCodes.CREATED).json({ product })
+
+}
+const getAllProduct = async (req, res) => {
+    res.send('list of products')
+}
+
+module.exports = {
+    createProduct,
+    getAllProduct
+}
