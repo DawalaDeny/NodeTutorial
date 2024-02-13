8d270f7baba64cb369a3a0c2b60ee3c2d1c4ec12
denyshabouev <151159372+denyshabouev@users.noreply.github.com>
2024-02-13 14:39:31 +0100

    fileupload 07 gedaan

--- node-express-course-main/07-file-upload/starter/controllers/uploadsController.js
+++ node-express-course-main/07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,34 @@
+const path = require('path')
 const {StatusCodes} = require('http-status-codes')
+const customError = require('../errors/index')
+const cloudinary = require('cloudinary').v2
+const fs = require('fs')
 
-const uploadProductImage = async (req, res) => {
-    console.log(req);
-    res.send('upload product image')
+const uploadProductImageLocal = async (req, res) => {
+    if (!req.files){
+        throw new customError.BadRequestError('no file uploaded')
+    }
+    const image = req.files.image;
+    if (!image.mimetype.startsWith('image')){
+        throw new customError.BadRequestError('please upload an image')
+    }
+    const maxSize = 1024*1024
+    if (image.size > maxSize){
+        throw new customError.BadRequestError(`please upload something smaller than ${maxSize} kb`)
+    }
+
+    const imagePath = path.join(__dirname,'../public/uploads/' + `${image.name}` );
+await image.mv(imagePath);
+    return res.status(StatusCodes.OK).json({image:{src:`/uploads/${image.name}`}})
+}
+
+const uploadProductImage= async (req, res) => {
+  
+const result = await cloudinary.uploader.upload(req.files.image.tempFilePath,{
+    use_filename:true, folder:'07-file-upload'
+})
+fs.unlinkSync(req.files.image.tempFilePath)
+return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
 }
 
 module.exports = {
