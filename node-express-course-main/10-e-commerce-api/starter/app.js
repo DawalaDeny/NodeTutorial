23126a4de352616bc116cec3326dc7f17a6e0f6c
DawalaDeny <shabouevdeny@gmail.com>
2024-02-14 17:25:35 +0100

    start webshop express server + db connectie

--- node-express-course-main/10-e-commerce-api/starter/app.js
+++ node-express-course-main/10-e-commerce-api/starter/app.js
@@ -1,1 +1,22 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+//db
+const connectDb = require('./db/connect')
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URL)
+    app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
