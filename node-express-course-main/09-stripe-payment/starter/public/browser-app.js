d82fbdba76f0c2e4238f0d146a01b5af350798ce
denyshabouev <151159372+denyshabouev@users.noreply.github.com>
2024-02-14 14:34:55 +0100

    stripe payment gedaan

--- node-express-course-main/09-stripe-payment/starter/public/browser-app.js
+++ node-express-course-main/09-stripe-payment/starter/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51OjhabKFhDZeP3SypbpOyZjU5fxKQCmTKfxLpIUTXhCieCQQqS02YQKYLhE4D2OCTVH0wo8dCXVC94lCJVoYe7bg00UOy1kqGh'
 );
 
 // The items the customer wants to buy
